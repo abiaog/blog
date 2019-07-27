04a5a1f1f75b0f9fba983c0ccac34a1a60382ee7
abiaog <abiaog@126.com>
2019-07-27 17:32:15 +0800

    pmp

--- push.sh
+++ push.sh
@@ -1,38 +1,38 @@
 
-git add \
-	_posts/2016-02-12-rootfs.markdown\
-	_posts/2016-02-12-vim-introduction.markdown\
-	_posts/2016-02-12-welcome-to-jekyll.markdown\
-	_posts/2016-02-13-c++-Multiple-and-Virtual-Inheritance.markdown\
-	_posts/2016-02-13-Markdown.markdown\
-	_posts/2016-02-14-device-tree.markdown\
-	_posts/2016-02-14-u-boot.markdown\
-	_posts/2016-02-14-yocto.markdown\
-	_posts/2016-02-17-Erlang.markdown\
-	_posts/2016-02-17-google-test.markdown\
-	_posts/2016-05-05-gdb.markdown\
-	_posts/2016-07-27-bash.markdown\
-	_posts/2016-08-06-how-to-replace-ubuntu-kernel.markdown\
-	_posts/2016-08-26-linux-device-driver-hello-world.markdown\
-	_posts/2016-09-03-configure-mutt-msmtp.markdown\
-	_posts/2016-09-03-tcpdump-wireshark.markdown\
-	_posts/2016-09-17-bitbake.markdown\
-	_posts/2016-10-20-spinlock-seqlock-RCU-lock.markdown\
-	_posts/2016-11-11-Linux-Device-Driver-Advanced-Char-Driver.markdown\
-	_posts/2016-12-27-i2c-driver.markdown\
-	_posts/2017-01-18-QEMU.markdown\
-	_posts/2017-01-23-mutex-semaphore.markdown\
-	_posts/2017-02-14-Understanding-the-Linux-Kernel.markdown\
-	_posts/2017-03-03-JaveScript.markdown\
-	_posts/2017-03-07-exokernel-microkernel.markdown\
-	_posts/2017-03-22-c-language.markdown\
-	_posts/2017-03-22-makefile.markdown\
-	_posts/2017-04-28-xilinx.markdown\
-	_posts/2017-05-03-FreeRTOS.markdown\
-	_posts/2017-05-27-tips-and-tools.markdown\
-	_posts/2017-08-04-Understanding-the-Linux-Virtual-Memory-Manager.markdown\
-	_posts/2017-09-13-Docker.markdown\
-	_posts/2019-03-27-Project-Management-Programming.markdown\
+git add                                                                   \ 
+_posts/2016-02-12-rootfs.markdown                                         \ 
+_posts/2016-02-12-vim-introduction.markdown                               \ 
+_posts/2016-02-12-welcome-to-jekyll.markdown                              \ 
+_posts/2016-02-13-c++-Multiple-and-Virtual-Inheritance.markdown           \ 
+_posts/2016-02-13-Markdown.markdown                                       \ 
+_posts/2016-02-14-device-tree.markdown                                    \ 
+_posts/2016-02-14-u-boot.markdown                                         \ 
+_posts/2016-02-14-yocto.markdown                                          \ 
+_posts/2016-02-17-Erlang.markdown                                         \ 
+_posts/2016-02-17-google-test.markdown                                    \ 
+_posts/2016-05-05-gdb.markdown                                            \ 
+_posts/2016-07-27-bash.markdown                                           \ 
+_posts/2016-08-06-how-to-replace-ubuntu-kernel.markdown                   \ 
+_posts/2016-08-26-linux-device-driver-hello-world.markdown                \ 
+_posts/2016-09-03-configure-mutt-msmtp.markdown                           \ 
+_posts/2016-09-03-tcpdump-wireshark.markdown                              \ 
+_posts/2016-09-17-bitbake.markdown                                        \ 
+_posts/2016-10-20-spinlock-seqlock-RCU-lock.markdown                      \ 
+_posts/2016-11-11-Linux-Device-Driver-Advanced-Char-Driver.markdown       \ 
+_posts/2016-12-27-i2c-driver.markdown                                     \ 
+_posts/2017-01-18-QEMU.markdown                                           \ 
+_posts/2017-01-23-mutex-semaphore.markdown                                \ 
+_posts/2017-02-14-Understanding-the-Linux-Kernel.markdown                 \ 
+_posts/2017-03-03-JaveScript.markdown                                     \ 
+_posts/2017-03-07-exokernel-microkernel.markdown                          \ 
+_posts/2017-03-22-c-language.markdown                                     \ 
+_posts/2017-03-22-makefile.markdown                                       \ 
+_posts/2017-04-28-xilinx.markdown                                         \ 
+_posts/2017-05-03-FreeRTOS.markdown                                       \ 
+_posts/2017-05-27-tips-and-tools.markdown                                 \ 
+_posts/2017-08-04-Understanding-the-Linux-Virtual-Memory-Manager.markdown \ 
+_posts/2017-09-13-Docker.markdown                                         \ 
+_posts/2019-03-27-Project-Management-Programming.markdown                 \ 
 
 git add	push.sh
 git add	index.md
